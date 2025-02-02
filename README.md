f7bc7df880e1d9f033ef02f17cd3331dcd8c345e
farshed <faisalarshed28@gmail.com>
2025-02-03 04:50:29 +0500

    Create tmp and compile rust projects in setup.py

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Local, lightweight voice chat with LLMs.
 
+## Components
+
+-  Whisper.cpp for ASR
+-  Deepseek V3 for text gen
+-  Kokoro v0.19 for speech gen
+-  Bun & Elysia to tie all the pieces together
+
 ## Requirements
 
 -  [Rust](https://www.rust-lang.org/tools/install)
@@ -11,9 +18,11 @@ Local, lightweight voice chat with LLMs.
 ## Run
 
 1. Run `python setup.py` in the project root. This will download the required model weights and compile the binaries needed for Sage.
-2. Place a .env file containing `TOGETHER_API_KEY` in the project root directory.
+2. Place a .env file containing `TOGETHER_API_KEY` in the project root.
 3. Start the project with `bun dev`.
 
-## Pipeline
+## Future work
 
-🗣️ -> Speech -> Whisper.cpp -> transcript -> LLM -> text response -> Kokoro v0.19 -> vocal response
+1. Optimize the pipeline.
+2. Make it easier to run. (Dockerize?)
+3. Release as a library?
--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 import os
+import subprocess
 import urllib.request
 
-MODELS_DIR = './models'
+MODELS_DIR = 'models'
+DIRS = [MODELS_DIR, 'tmp/input', 'tmp/output', 'tmp/transcripts']
+
+def create_dirs():
+    for dir_path in DIRS:
+        os.makedirs(dir_path, exist_ok=True)
+
 FILES = [
     'https://hf-mirror.com/hexgrad/Kokoro-82M/resolve/main/kokoro-v0_19.onnx',
     'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-base.en.bin',
     
 ]
 
-# create tmp dirs
-# compile binaries
-
 def download_files():
-    if not os.path.exists(MODELS_DIR):
-        os.makedirs(MODELS_DIR)
-
     for url in FILES:
         try:
             filename = os.path.join(MODELS_DIR, url.split("/")[-1])
@@ -25,4 +26,21 @@ def download_files():
         except Exception as e:
             print(f"Error downloading {url}: {e}")
 
+def compile_rust_project(project_path):
+    try:
+        if not os.path.isdir(project_path):
+            print(f"Error: Project not found at '{project_path}'")
+            return
+
+        result = subprocess.run(['cargo', 'build', '--release'], cwd=project_path, capture_output=True, text=True, check=True)
+        
+        print("Build successful.")
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Build failed: {e}. Check if you have rust installed.")
+        print(e.stderr) 
+
+create_dirs()
 download_files()
+compile_rust_project('kokoro')
+compile_rust_project('whisper')
